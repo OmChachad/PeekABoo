e8a3dfa480103a8c52ee5bcfd18d3e020c96df66
Om Chachad <96822675+OmChachad@users.noreply.github.com>
2025-11-24 14:52:36 -0800

    Initial project setup for PeekABoo app
    Added RealityKitContent Swift package, assets, and configuration files. Set up Xcode project, workspace, and basic SwiftUI ContentView displaying a RealityKit 3D scene. Includes initial Info.plist, README, and RealityKit asset files.

---
+++ PeekABoo/PeekABooApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeekABooApp.swift
+//  PeekABoo
+//
+//  Created by Om Chachad on 23/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PeekABooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
