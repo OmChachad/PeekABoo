ef49788344e77d5fea2566b3a3da9cef0aefdb49
Om Chachad <omchachad12@gmail.com>
2026-03-15 14:05:29 -0700

    Added a way to request and check for Photo Library access

--- Sources/PeekABoo/PeekABoo.swift
+++ Sources/PeekABoo/PeekABoo.swift
@@ -6,6 +6,25 @@ import Photos
 
 // MARK: - Public API
 
+public enum PeekABoo {
+    /// Requests full access to the photo library.
+    ///
+    /// The user will be prompted to grant access if they haven't already.
+    /// Returns `true` if full access is granted, `false` if access is limited or denied.
+    @MainActor
+    public static func requestAccess() async -> Bool {
+        await PhotoLibraryManager.requestAccess()
+    }
+
+    /// Checks if full access to the photo library has been granted.
+    ///
+    /// This returns `true` only if the user has granted full "All Photos" access.
+    /// It returns `false` if the user has granted "Limited" or "No" access.
+    public static var isAccessGranted: Bool {
+        PHPhotoLibrary.authorizationStatus(for: .readWrite) == .authorized
+    }
+}
+
 public extension View {
     /// Calls the completion handler with a `UIImage` whenever a new screenshot is detected.
     ///
