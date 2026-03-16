9f3cf92ce5ef298f19c34245a7c52180e0267d3e
Om Chachad <omchachad12@gmail.com>
2026-03-15 22:24:46 -0700

    Added Sample Project (Demo State)

---
+++ Examples/QRCodeScanner/QRCodeScanner/QRCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeScannerApp.swift
+//  QRCodeScanner
+//
+//  Created by Om Chachad on 15/03/26.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
